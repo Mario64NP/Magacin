f42eee0df196740dbf3f3dc22ca9b2f87e81f8ed
Mario64NP <mj20171055@student.fon.bg.ac.rs>
2022-03-16 19:26:03 +0100

    Dodata klasa Magacin

---
+++ Magacin/src/Magacin.java
@@ -0,0 +1,23 @@
+import artikal.Artikal;
+import specifikacijainterfejsa.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs{
+
+	@Override
+	public void dodajArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbacArtkal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal nadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
